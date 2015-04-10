5d803209e1eccbcb52c0f513dfe5c17a36d63e43
Jay Renteria <jay@jayrenteria.com>
2015-04-10 15:53:49 -0600

    working to get the full scss parallax implemented

---
+++ jay.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" />
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
+
+		<title>Parallax Example</title>
+
+		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="test.css">
+	</head>
+	<body>
+
+		<div class="parallax">
+			<div class="parallax__layer parallax__layer__0">
+				<img src="img/top.png">
+			</div>
+			<div class="parallax__layer parallax__layer__1">
+				<img src="img/next-middle.png">
+			</div>
+			<div class="parallax__layer parallax__layer__2">
+				<img src="img/top-middle.png">
+			</div>
+			<div class="parallax__layer parallax__layer__3">
+				<img src="img/bottom-middle.png">
+			</div>
+			<div class="parallax__layer parallax__layer__4">
+				<img src="img/bottom.png">
+			</div>
+<!--			<div class="parallax__layer parallax__layer__5">-->
+<!--			</div>-->
+<!--			<div class="parallax__layer parallax__layer__6">-->
+<!--			</div>-->
+			<div class="parallax__cover"></div>
+		</div>
+
+
+
+	</body>
+</html>
